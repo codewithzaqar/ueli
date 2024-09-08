a89273b97bfb21dfc38edc445111c467708c6a23
PerfectDevloper <hakobyanzaqar15@gmail.com>
2024-09-08 11:28:38 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+var { app, globalShortcut, BrowserWindow } = require('electron')
+
+var mainWindow = null
+
+app.on('window-all-closed', function() {
+    if(process.platform != 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 200,
+        frame: false
+    })
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html')
+
+    mainWindow.on('closed', function() {
+        mainWindow = null
+    })
+
+    globalShortcut.register('alt+space', function () {
+        if(mainWindow.isVisible())
+            mainWindow.hide()
+        else
+            mainWindow.show()
+    })
+})
